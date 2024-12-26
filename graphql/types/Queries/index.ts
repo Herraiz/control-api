200df69c448b9445fa55201a5964ba000fd5b5ac
Rober Herraiz <herraiz92@gmail.com>
2024-12-26 10:16:14 +0000

    feat: added plural queries

--- graphql/types/Queries/index.ts
+++ graphql/types/Queries/index.ts
@@ -3,8 +3,11 @@ export { default as me } from "./me";
 
 // Public Queries
 export { default as getBudget } from "./getBudget";
+export { default as getBudgets } from "./getBudgets";
 export { default as getDebt } from "./getDebt";
+export { default as getDebts } from "./getDebts";
 export { default as getTransaction } from "./getTransaction";
+export { default as getTransactions } from "./getTransactions";
 
 // Private Queries
 export { default as _canDoAction } from "./_canDoAction";
