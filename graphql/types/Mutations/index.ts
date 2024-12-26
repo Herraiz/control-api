3228b92c7f6b511cc527408c9291a37544c0c04a
Rober Herraiz <herraiz92@gmail.com>
2024-12-26 11:56:21 +0000

    chore: now mutations are grouped by authorizations

--- graphql/types/Mutations/index.ts
+++ graphql/types/Mutations/index.ts
@@ -1,12 +1,13 @@
 // 🌎 Public Mutations
-export { default as login } from "./login";
-export { default as registerUser } from "./registerUser";
-export { default as requestResetPassword } from "./requestResetPassword";
-export { default as resetPasswordWithToken } from "./resetPasswordWithToken";
+export { default as login } from "./public/login";
+export { default as registerUser } from "./public/registerUser";
+export { default as requestResetPassword } from "./public/requestResetPassword";
+export { default as resetPasswordWithToken } from "./public/resetPasswordWithToken";
 
 // 👨‍💻 User Mutations
-export { default as renewToken } from "./renewToken";
-export { default as updateUser } from "./updateUser"; // TODO: Register ActivityLog
+export { default as renewToken } from "./user/renewToken";
+export { default as updateUser } from "./user/updateUser"; // TODO: Register ActivityLog
+export { default as deleteUser } from "./user/requestUserDelete";
 
 // 💰 Budget Mutations
 export { default as createBudget } from "./budget/createBudget";
@@ -16,6 +17,9 @@ export { default as deleteBudget } from "./budget/deleteBudget";
 // 💸 Debt Mutations
 
 // 📊 Transaction Mutations
+
+// 👨‍💼 Admin Mutations
+export { default as changeUserPassword } from "./admin/changeUserPassword";
 
 // 🙈 Private Mutations
 export { default as _registerActivityLog } from "./_registerActivityLog";
