1f855d9a4d2ebb28dab31c4f85eea3b401f3be7c
Rober Herraiz <herraiz92@gmail.com>
2025-02-14 11:36:42 +0000

    chore: finished transactions front modifications

--- graphql/types/Queries/getTransactions.ts
+++ graphql/types/Queries/getTransactions.ts
@@ -1,4 +1,4 @@
-import { nonNull, queryField, stringArg, list } from "nexus";
+import { nonNull, queryField, stringArg, list, arg } from "nexus";
 import {
   authorizeFieldCurrentUser,
   orComposeAuthorize,
@@ -9,15 +9,19 @@ export default queryField("getTransactions", {
   type: list("Transaction"),
   args: {
     userId: nonNull(stringArg()),
+    orderBy: arg({ type: "String", default: "asc" }), // Argumento opcional para ordenar
   },
   authorize: orComposeAuthorize(
     authorizeFieldCurrentUser,
     authorizeFieldUserIsAdmin,
   ),
-  resolve: async (_, { userId }, ctx) => {
+  resolve: async (_, { userId, orderBy }, ctx) => {
     return ctx.prisma.transaction.findMany({
       where: {
         userId,
+      },
+      orderBy: {
+        createdAt: orderBy === "desc" ? "desc" : "asc", // Ordenar por fecha de creación
       },
     });
   },
