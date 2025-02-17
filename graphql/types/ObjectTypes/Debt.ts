ba05afe806e39f8956c75ec4b99b4c9c67b4bbb6
Rober Herraiz <herraiz92@gmail.com>
2025-02-17 10:32:44 +0000

    chore: added remainedAmount to Debt graphql object

--- graphql/types/ObjectTypes/Debt.ts
+++ graphql/types/ObjectTypes/Debt.ts
@@ -1,3 +1,4 @@
+import { InterestType } from "@prisma/client";
 import { objectType } from "nexus";
 
 export default objectType({
@@ -36,6 +37,27 @@ export default objectType({
             debtId: parent.id,
           },
         });
+      },
+    });
+
+    // calculated remainingAmount -> calculate interest amount and sum with totalAmount
+    t.float("remainingAmount", {
+      resolve: async (parent, _args, ctx) => {
+        let remainingAmount = parent.totalAmount;
+        const interestRate = parent.interestRate / 100;
+
+        if (parent.interestType === InterestType.TIN) {
+          // Calculate simple interest
+          const interestAmount = parent.totalAmount * interestRate;
+          remainingAmount += interestAmount;
+        } else if (parent.interestType === InterestType.TAE) {
+          // Calculate compound interest
+          const periods = parent.paymentYears;
+          remainingAmount =
+            parent.totalAmount * Math.pow(1 + interestRate, periods);
+        }
+
+        return remainingAmount;
       },
     });
 
