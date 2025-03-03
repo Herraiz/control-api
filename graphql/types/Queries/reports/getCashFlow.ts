720140dfe3dc84cf4d62e452c3e02ee8c7400520
Rober Herraiz <herraiz92@gmail.com>
2025-03-03 13:02:31 +0000

    chore: added optional args to getCashFlow query

--- graphql/types/Queries/reports/getCashFlow.ts
+++ graphql/types/Queries/reports/getCashFlow.ts
@@ -1,4 +1,4 @@
-import { list, nonNull, queryField, stringArg } from "nexus";
+import { list, nonNull, nullable, queryField, stringArg } from "nexus";
 import { ApolloError } from "apollo-server-micro";
 import { authorizeFieldCurrentUser } from "@/graphql/utils";
 
@@ -6,16 +6,59 @@ export default queryField("getCashFlow", {
   type: list("CashFlow"),
   args: {
     userId: nonNull(stringArg()),
+    year: nullable(stringArg()),
+    month: nullable(stringArg()),
   },
   authorize: authorizeFieldCurrentUser,
 
-  resolve: async (_, { userId }, ctx) => {
+  resolve: async (_, { userId, year, month }, ctx) => {
     if (userId !== ctx.user.id) {
       throw new ApolloError("Unauthorized", "UNAUTHORIZED_NOT_SAME_USER");
     }
 
     const now = new Date();
     const cashFlowData = [];
+
+    if (year && month) {
+      const monthStart = new Date(parseInt(year), parseInt(month), 1);
+      const monthEnd = new Date(parseInt(year), parseInt(month) + 1, 1);
+
+      const expenses = await ctx.prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: "EXPENSE",
+          date: {
+            gte: monthStart,
+            lt: monthEnd,
+          },
+        },
+        _sum: {
+          amount: true,
+        },
+      });
+
+      const incomes = await ctx.prisma.transaction.aggregate({
+        where: {
+          userId,
+          type: "INCOME",
+          date: {
+            gte: monthStart,
+            lt: monthEnd,
+          },
+        },
+        _sum: {
+          amount: true,
+        },
+      });
+
+      return [
+        {
+          month: parseInt(month),
+          incomes: incomes._sum.amount || 0,
+          expenses: expenses._sum.amount || 0,
+        },
+      ];
+    }
 
     for (let i = 0; i < 12; i++) {
       const monthStart = new Date(now.getFullYear(), now.getMonth() - i, 1);
